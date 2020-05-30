09fd71ee9cb2ebfc4d2f67578b43aa53ec8e6516
thomasneo <thomaslossio@hotmail.com>
2020-05-30 10:35:33 -0300

    Add username and pass on Login

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h1>Login</h1>
+    <div>
+      <label>Username:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>Password:</label>
+      <input type="password" autocomplete="new-password" />
+    </div>
+    <button>Login</button>
   </div>
 </template>
 
